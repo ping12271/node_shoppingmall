17a18815ed9b6609d010ba5bdcef69eae8cad3d1
lala <ping12271@hanmail.net>
2020-11-05 21:07:28 +0900

    req, res, test

--- app.js
+++ app.js
@@ -4,11 +4,23 @@
 // 2. 위에서 아래로 읽힌다.
 // 3. .은 function으로 불러온다.
 
-const express = require('express')
+const express = require('express') //require : 요구한다, express를 불러온다.
 const app = express() //express의 일반적인 기능들의 모음
+
+
+app.use((req, res) => {
+    res.json({
+        "message" : "서버응답했음"
+    })
+})//req : 사용자 요청(받기) / res : 서버 응답(보내기)
+
+
+
+
+
+
 
 const port = 9999 //9999를 통해서 통신을 하겠다.
 
-
-app.listen(port, console.log("server started"))//app=express()을 실행한다 -> listen
+app.listen(port, console.log("server started"))//listen(함수)-> app을 실행하겠다는 메서드(방법), port를 통해서만 실행하겠다.
 
