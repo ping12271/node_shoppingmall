ce56c37f276495679f6bca67d680bd96ada0584f
lala <ping12271@hanmail.net>
2020-11-16 22:05:57 +0900

    add : user sign up

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,43 @@
 const exress = require('express')
 const router = exress.Router()
+const userModel = require('../models/user')
+
+
+
+// 회원가입 API
+router.post('/register', (req, res) => {
+    
+
+    // 기존 이메일 유무체크 -> 패스워드 암호화 -> db에 저장
+
+    const newUser = new userModel({
+        username : req.body.username,
+        email : req.body.email,
+        password : req.body.password
+    })
+
+    newUser
+        .save()
+        .then(user => {
+            res.json({
+                message : 'saved data',
+                userInfo : user
+            })
+        })
+        .catch(err => {
+            res.json({
+                message : err
+            })
+        })
+})
+
+// 로그인 API
+router.post('/login', (req, res) => {
+
+})
+
+
+
 
 
 module.exports = router
