ba6d287761bd6d848c7f0c4d1b16eb171a824fd8
lala <ping12271@hanmail.net>
2020-11-19 20:55:45 +0900

    add : 패스워드 암호화

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const exress = require('express')
 const router = exress.Router()
+const bcrypt = require('bcryptjs')
 const userModel = require('../models/user')
 
 
@@ -7,28 +8,35 @@ const userModel = require('../models/user')
 // 회원가입 API
 router.post('/register', (req, res) => {
     
+    //기존 이메일 유무체크 -> 패스워드 암호화 -> db에 저장
 
-    // 기존 이메일 유무체크 -> 패스워드 암호화 -> db에 저장
-
-    const newUser = new userModel({
-        username : req.body.username,
-        email : req.body.email,
-        password : req.body.password
-    })
-
-    newUser
-        .save()
-        .then(user => {
-            res.json({
-                message : 'saved data',
-                userInfo : user
-            })
-        })
-        .catch(err => {
-            res.json({
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if(err) {
+            return res.json({
                 message : err
             })
-        })
+        } else {
+            const newUser = new userModel({
+                username : req.body.username,
+                email : req.body.email,
+                password : hash
+            })
+        
+            newUser
+                .save()
+                .then(user => {
+                    res.json({
+                        message : 'saved data',
+                        userInfo : user
+                    })
+                })
+                .catch(err => {
+                    res.json({
+                        message : err
+                    })
+                })
+        }
+    })
 })
 
 // 로그인 API
